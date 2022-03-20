4c20ac6d379e12f908ae04ab6199ea8e280351ca
choi-byoungjin <choibyoungjin85@gmail.com>
2022-03-20 18:34:25 +0900

    테스트

---
+++ AnnaSemiProject/WebContent/WEB-INF/sql/AnnaSemiProject.sql
@@ -0,0 +1,11 @@
+create table anna_test
+(seq          number
+,name         varchar2(20) not null 
+);
+
+show user
+
+select * from anna_test
+
+INSERT into anna_test (seq, name)  values (1, 'choi');
+
