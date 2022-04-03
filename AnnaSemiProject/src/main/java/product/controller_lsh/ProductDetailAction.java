0749eb8c72e72c3d0b8813fda096981a288366e7
Helenhyerim hr4658@naver.com <sist@LAPTOP-U5OIAH7T>
2022-04-03 22:48:22 +0900

    userid get방식으로 넘기는 setAttribute 추가

--- AnnaSemiProject/src/main/java/product/controller_lsh/ProductDetailAction.java
+++ AnnaSemiProject/src/main/java/product/controller_lsh/ProductDetailAction.java
@@ -4,11 +4,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import common.controller.AbstractController;
+import product.model_lsh.InterProductDAO;
+import product.model_lsh.ProductDAO;
+import product.model_lsh.ProductVO;
 
 public class ProductDetailAction extends AbstractController {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+			String productnum = request.getParameter("productnum");
+		//	System.out.println("확인용 productnum =>" +productnum);
+			
+			request.setAttribute("productnum", productnum);
+			
+			InterProductDAO pdao = new ProductDAO();
+			ProductVO pvo = pdao.getProductDetail(productnum);
 		
 		//	super.setRedirect(false);
 			super.setViewPage("/WEB-INF/product_lsh/productDetail.jsp");
