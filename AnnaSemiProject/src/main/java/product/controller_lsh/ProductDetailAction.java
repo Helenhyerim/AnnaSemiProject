373f8ed0ff68d7179293bac7e9a50a07bbdeb3c5
lsh87 <stjdgml6763@naver.com>
2022-04-05 15:03:17 +0900

    productDetail 수정, MyUtil 클래스 생성

--- AnnaSemiProject/src/main/java/product/controller_lsh/ProductDetailAction.java
+++ AnnaSemiProject/src/main/java/product/controller_lsh/ProductDetailAction.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import common.controller.AbstractController;
+import my.util.MyUtil;
 import product.model_lsh.InterProductDAO;
 import product.model_lsh.ProductDAO;
 import product.model_lsh.ProductVO;
@@ -19,7 +20,13 @@ public class ProductDetailAction extends AbstractController {
 			
 			InterProductDAO pdao = new ProductDAO();
 			ProductVO pvo = pdao.getProductDetail(productnum);
-		
+
+			// 로그인 후 이용가능한 기능을 로그인하지 않은 사용자가 이용하려고 하는 경우
+			// 로그인 페이지로 이동 후 다시 제품상세페이지로 돌아오기 위한 것
+			String currentURL = MyUtil.getCurrentURL(request);
+			
+			request.setAttribute("goBackURL", currentURL);
+			
 		//	super.setRedirect(false);
 			super.setViewPage("/WEB-INF/product_lsh/productDetail.jsp");
 		
